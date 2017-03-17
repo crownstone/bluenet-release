f8be6a8fcd225394038128566c5847af15d6e746
Vliedel <bart@dobots.nl>
2017-03-17 19:40:03 +0100

    added 1.3.1

---
+++ crownstone_1.3.1/docs/html/search/classes_e.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['reply_5fmessage_5ft',['reply_message_t',['../structreply__message__t.html',1,'']]],
+  ['rng',['RNG',['../classRNG.html',1,'']]],
+  ['rtc',['RTC',['../classRTC.html',1,'']]]
+];
---
+++ crownstone_1.3.1/docs/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['begin',['begin',['../classstate__message.html#a6ea6ec699ff606d8baa754b1037d0be2',1,'state_message']]]
+];
